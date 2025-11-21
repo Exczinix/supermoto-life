204f382a7afc567a8d3b81e47ffe737c9551eb51
Exczinix <dj.zinix@gmail.com>
2025-11-21 23:32:51 +0100

    Delete supermoto site directory

--- supermoto site/js/script.js
+++
@@ -1,9 +0,0 @@
-// Hamburger menu
-const mobileMenu = document.getElementById('mobile-menu');
-const nav = document.querySelector('nav ul');
-
-if(mobileMenu) {
-    mobileMenu.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
-}
